17ae962accf5607d2497caa09751f1060b31147e
Vitor Santos <vitoralannl@gmail.com>
2022-05-03 09:48:56 -0300

    feat (Cofo.h) - Cabeçalho do TAD Cofo

---
+++ trabalho_implementacao_01/Cofo.h
@@ -0,0 +1,24 @@
+//
+// Created by vitor on 29/04/22.
+//
+
+#ifndef TRABALHO_IMPLEMENTACAO_01_COFO_H
+#define TRABALHO_IMPLEMENTACAO_01_COFO_H
+
+typedef struct cofo Cofo;
+
+int find_cofo(Cofo *c, int key );
+
+int insert_cofo(Cofo *c, int item);
+
+Cofo *create_cofo(int size);
+
+int *itens_cofo(Cofo *cofo);
+
+int delete_cofo(Cofo *c);
+
+int remove_item_cofo(Cofo *cofo, int key);
+
+int get_max_itens(Cofo *cofo);
+
+#endif //TRABALHO_IMPLEMENTACAO_01_COFO_H
