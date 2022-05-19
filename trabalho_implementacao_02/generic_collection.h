01b618c1c2a1228bc6c899df52dc21bbc801df68
Vitor Santos <vitoralannl@gmail.com>
2022-05-19 17:35:18 -0300

    feat (generic_collection.h) [trabalho_implementacao_02]
    Generic collection headers file

---
+++ trabalho_implementacao_02/generic_collection.h
@@ -0,0 +1,24 @@
+//
+// Created by vitor on 17/05/22.
+//
+
+#ifndef GENERIC_COLLECTION_H_INCLUDED
+#define GENERIC_COLLECTION_H_INCLUDED
+
+typedef struct generic_collection GenericCollection;
+
+GenericCollection *genericCollectionCreate(int size);
+
+int genericCollectionDestroy(GenericCollection *genericCollection);
+
+int genericCollectionInsert(GenericCollection *genericCollection, void *element);
+
+void *genericCollectionFindAll(GenericCollection *genericCollection);
+
+void *genericCollectionFind(GenericCollection *genericCollection, void *key, int(*cmp)(void *, void *));
+
+int genericCollectionRemove(GenericCollection *genericCollection, void *key, int(*cmp)(void *, void *));
+
+int genericCollectionEmpty(GenericCollection *genericCollection);
+
+#endif //GENERIC_COLLECTION_H_INCLUDED
